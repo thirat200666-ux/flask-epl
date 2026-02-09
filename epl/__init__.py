0791b73268359e4ca36c53e3077dcab6496c4fd3
thirat200666-ux <thirat200666@gmail.com>
2026-02-09 14:38:49 +0700

    WEEK11

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite'
+app.secret_key = b'hgyuftrdfikkjhtdfyui'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# ❌ คอมเมนต์ไว้ก่อน
-# from epl import routes, models
+from epl import routes, models
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from epl import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
